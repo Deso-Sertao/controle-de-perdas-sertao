2444bd2bf21d13d2156e63add473f9f41cd60c7c
Deso Sertão <35984838+Deso-Sertao@users.noreply.github.com>
2019-08-14 16:04:19 -0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRvhiAVg8gPmOaEjDtjzTTPjJz7l1U9jYLKIE4YhxX1iN9Z27GzV0FNIKR9FNNJiXxgQJIplKGuKe0n/pubhtml?gid=164271551&single=true';
